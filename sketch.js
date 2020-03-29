a5c13b5d3fd7d13df370c2e171cbe33de837ea59
Jem Altieri <jem.altieri@newsela.com>
2020-03-29 02:11:48 -0400

    refactor

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
-const CANVAS_WIDTH = screen.width;
-const CANVAS_HEIGHT = screen.height;
 const NUM_OCTAVES = 5;
-const SAMPLE_RATE = 192000
 const FFT_SIZE = 16384;
 const DAMPING = 50;
-const WIGGLE_AMPLITUDE = 0.4;
-const FREQUENCY_AT_ZERO_THETA = 120;
+const WIGGLE_AMPLITUDE = 1.5;
+const FREQUENCY_AT_ZERO_THETA = 87.3;
+const MIN_FREQ = 87.3;
+const COLOR_OFFSET = 236;
+const MAX_FREQ = MIN_FREQ * Math.pow(2, NUM_OCTAVES);
+const MIN_PITCH = Math.log2(MIN_FREQ);
+const MAX_PITCH = Math.log2(MAX_FREQ);
+const PITCH_AT_ZERO_THETA = Math.log2(FREQUENCY_AT_ZERO_THETA);
+const FFT_SMOOTHING = 0.8;
 
 function CarPoint(x, y) {
   this.x = x;
@@ -51,43 +55,51 @@ function polarLine(polPoint1, polPoint2, origin) {
 var mic;
 var fft;
 
+var hzPerBin;
+var minBin;
+var maxBin;
+var canvasWidth;
+var canvasHeight;
+
 function setup() {
   fullscreen();
-  let cnv = createCanvas(screen.width, screen.height);
+  let cnv = createCanvas(windowWidth, windowHeight);
   cnv.mousePressed(userStartAudio);
   background(0);
   mic = new p5.AudioIn();
   mic.start();
-  fft = new p5.FFT(0.8, FFT_SIZE);
+  fft = new p5.FFT(FFT_SMOOTHING, FFT_SIZE);
   fft.setInput(mic);
+  hzPerBin = sampleRate() / (FFT_SIZE * 2);
+  minBin = Math.round(MIN_FREQ / hzPerBin);
+  maxBin = Math.round(MAX_FREQ / hzPerBin);
+  canvasWidth = windowWidth;
+  canvasHeight = windowHeight;
 }
 
 function draw() {
   let spectrum = fft.analyze();
-  const HZ_PER_BIN = SAMPLE_RATE / (FFT_SIZE * 2);
-  const MIN_FREQ = 120.;
-  const MAX_FREQ = MIN_FREQ * Math.pow(2, NUM_OCTAVES);
-  const MIN_BIN = Math.round(MIN_FREQ / HZ_PER_BIN);
-  const MAX_BIN = Math.round(MAX_FREQ / HZ_PER_BIN);
-  let colorOffset = 240;
   colorMode(HSB, 255);
   background(0, DAMPING);
   strokeWeight(4);
   let minTheta = Math.PI * 3 / 2;
-  let maxR = CANVAS_HEIGHT/2;
-  let minR = CANVAS_HEIGHT/60;
-  let origin = new CarPoint(CANVAS_WIDTH/2, CANVAS_HEIGHT/2);
-  let lastPoint = new PolPoint(minR, minTheta);
+  let maxR = canvasHeight/2 - canvasHeight/30;
+  let minR = canvasHeight/60;
+  let origin = new CarPoint(canvasWidth/2, canvasHeight/2);
+  let lastPoint;
 
-  for (let i = MIN_BIN; i <= MAX_BIN; i++) {
-    let freq = i * HZ_PER_BIN;
-    let note = Math.log2(freq / MIN_FREQ);
-    let percentWayThroughSpiral = note / NUM_OCTAVES;
-    let theta = minTheta + (note * Math.PI * 2);
-    let sprialR = minR + ((maxR - minR) * (note / NUM_OCTAVES))
-    let r = sprialR + (spectrum[i]*WIGGLE_AMPLITUDE*(percentWayThroughSpiral + 1));
+  for (let i = minBin; i <= maxBin; i++) {
+    let freq = i * hzPerBin;
+    let pitch = Math.log2(freq);
+    let theta = Math.PI * 2 * (pitch - PITCH_AT_ZERO_THETA);
+    let A = (maxR - minR) / NUM_OCTAVES;
+    let spiralR = (A * (pitch - MIN_PITCH)) + minR;
+    let wiggle = (spectrum[i] / 255) * WIGGLE_AMPLITUDE * A;
+    let r = spiralR + wiggle;
+    let percentWayThroughSpiral = (pitch - MIN_PITCH) / (MAX_PITCH - MIN_PITCH);
     let newPoint = new PolPoint(r, theta);
-    let h = (colorOffset + percentWayThroughSpiral * 255) % 255;
+    lastPoint = lastPoint || newPoint; // if it's our first point, let's just make it a single point
+    let h = (COLOR_OFFSET + percentWayThroughSpiral * 255) % 255;
     stroke(h, 255, 255);
     polarLine(lastPoint, newPoint, origin);
     lastPoint = newPoint;
@@ -97,4 +109,10 @@ function draw() {
 function mousePressed() {
   let fs = fullscreen();
   fullscreen(!fs);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  canvasWidth = windowWidth;
+  canvasHeight = windowHeight;
 }
